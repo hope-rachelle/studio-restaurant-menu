afd5db468722c7095b3c72c2e6763371051c507f
Hope Ciuffa <hopeciuffa@gmail.com>
2021-09-14 15:36:25 -0500

    menu items added

--- src/restaurant/Restaurant.java
+++ src/restaurant/Restaurant.java
@@ -2,10 +2,15 @@ package restaurant;
 
 public class Restaurant {
     public static void main(String[] args) {
-        MenuItem spaghetti = new MenuItem("Spaghetti", "Noodles with sauce","Main Course", 10.00);
         Menu restaurantMenu = new Menu();
-
+        MenuItem spaghetti = new MenuItem("Spaghetti", "Noodles with sauce","Main Course", 10.00);
+        MenuItem garlicBread = new MenuItem("Garlic Bread", "toasted garlic bread", "Appetizer", 7.25);
+        MenuItem cheesecake = new MenuItem("Cheesecake", "cheesecake", "Dessert", 6.50);
+        MenuItem chicken = new MenuItem("Chicken Parmesan", "breaded chicken", "Main Course", 15.00);
         restaurantMenu.addItem(spaghetti);
+        restaurantMenu.addItem(garlicBread);
+        restaurantMenu.addItem(cheesecake);
+        restaurantMenu.addItem(chicken);
 
         System.out.println(spaghetti.toString());
         System.out.println(restaurantMenu.toString());
